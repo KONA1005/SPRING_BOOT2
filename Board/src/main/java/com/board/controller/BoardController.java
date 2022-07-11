73bc85fa0f52bf1443e006235994046675d0b70b
DESKTOP-4QN0U18\home <sjkor1005@naver.com>
2022-07-11 14:26:52 +0900

    게시판 글쓰기 화면 진행중1

--- Board/src/main/java/com/board/controller/BoardController.java
+++ Board/src/main/java/com/board/controller/BoardController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.board.domain.BoardDTO;
 import com.board.service.BoardService;
 
 @Controller
@@ -17,16 +19,18 @@ public class BoardController {
 //	public String openBoardWrite(Model model) {
 //		return "board/write";
 //	} 아래걸로 변경
+	
 	@GetMapping(value = "/board/write.do")
-	public String openBoardWrite(Model model) {
-
-		String title = "제목";
-		String content = "내용";
-		String writer = "홍길동";
-
-		model.addAttribute("t", title);
-		model.addAttribute("c", content);
-		model.addAttribute("w", writer);
+	public String openBoardWrite(@RequestParam(value = "idx", required = false) Long idx, Model model) {
+		if (idx == null) {
+			model.addAttribute("board", new BoardDTO());
+		} else {
+			BoardDTO board = boardService.getBoardDetail(idx);
+			if (board == null) {
+				return "redirect:/board/list.do";
+			}
+			model.addAttribute("board", board);
+		}
 
 		return "board/write";
 	}
